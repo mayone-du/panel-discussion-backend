e101356b89679d2e30046d830f5ad039fb719386
mayone-du <cocomayo1201@icloud.com>
2021-05-03 22:23:20 +0900

    Bug fix

--- api/models.py
+++ api/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
 
+
 class User(AbstractBaseUser, PermissionsMixin):
   username = models.CharField(max_length=100, unique=True)
+  email = models.EmailField(blank=True, null=True)
+  first_name = models.CharField(max_length=50 ,blank=True, null=True)
+  last_name = models.CharField(max_length=50 ,blank=True, null=True)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+  objects = BaseUserManager()
 
   USERNAME_FIELD = 'username'
 
