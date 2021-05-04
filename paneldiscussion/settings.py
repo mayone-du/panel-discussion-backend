34b26544a3a1f71711d82533a729b9abebf78acc
mayone-du <cocomayo1201@icloud.com>
2021-05-04 09:57:33 +0900

    Fix secret settings

--- paneldiscussion/settings.py
+++ paneldiscussion/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2+@yzrd%fyhep^!hw__2a^o%7d#d-(gxnu!0ig^z8w^-*v9%g-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
