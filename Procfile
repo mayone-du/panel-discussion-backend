0abe866b546e3340f52e66882016ad301a8c1e88
mayone-du <cocomayo1201@icloud.com>
2021-05-06 10:33:28 +0900

    Setting deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn paneldiscussion.wsgi --log-file -
--- paneldiscussion/settings.py
+++ paneldiscussion/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,11 +97,10 @@ WSGI_APPLICATION = 'paneldiscussion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config("DATABASE_URL", default=default_dburl, cast=dburl)
 }
 
 
