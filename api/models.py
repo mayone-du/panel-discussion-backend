ab55b974be634a2a8d32458cd0c06fc419f36e64
mayone-du <cocomayo1201@icloud.com>
2021-05-06 12:14:31 +0900

    Fix UserManager

--- api/models.py
+++ api/models.py
@@ -4,6 +4,23 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 # Create your models here.
 
 
+class UserManager(BaseUserManager):
+  def create_user(self, username, password):
+    if not username:
+      raise ValueError('username is must')
+    
+    user = self.model(username=username)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+  def create_superuser(self, username, password):
+    user = self.create_user(username, password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.save(using=self._db)
+    return user
+
 class User(AbstractBaseUser, PermissionsMixin):
   username = models.CharField(max_length=100, unique=True)
   email = models.EmailField(blank=True, null=True)
@@ -11,7 +28,7 @@ class User(AbstractBaseUser, PermissionsMixin):
   last_name = models.CharField(max_length=50 ,blank=True, null=True)
   is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
-  objects = BaseUserManager()
+  objects = UserManager()
 
   USERNAME_FIELD = 'username'
 
